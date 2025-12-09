014bf40736be007bd595b22775476ff34f10b59b
Dhaval Kamariya <d.c.kamariya@gmail.com>
2025-12-09 08:14:32 +0530

    Add firebase config and initialize Firestore (Offline enabled)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,24 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { enableIndexedDbPersistence, getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => {
+      const db = getFirestore()
+      // enable offline persistence (best-effort; will silently fall back if not available)
+      enableIndexedDbPersistence(db).catch((err) => {
+        console.error('Error enabling offline persistence', err)
+      });
+
+      return db
+    })
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCHQMsZy19owCi5CtPyuAGJIJsKourciww",
+        authDomain: "actify-2001.firebaseapp.com",
+        projectId: "actify-2001",
+        storageBucket: "actify-2001.firebasestorage.app",
+        messagingSenderId: "105087541236",
+        appId: "1:105087541236:web:b1a13fdbf046e0a83863a1"
+    }
+}
